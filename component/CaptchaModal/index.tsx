0789158bd16b5769a8f2094a8049f6e42062791f
Ace+ <huangtaro58@gmail.com>
2023-03-13 20:15:50 +0800

    fix: second verification

--- component/CaptchaModal/index.tsx
+++ component/CaptchaModal/index.tsx
@@ -8,13 +8,12 @@ function isServe() {
 
 const googleVerify = (success?: () => void) => {
   if (!window.grecaptcha?.render) {
-    success?.()
     return
   }
 
   window.grecaptcha.render('robot', {
     sitekey: process.env.NEXT_PUBLIC_RECAPTCHA_WEB_SECRET,
-    callback: function (token: any) {
+    callback(token: any) {
       verify(token)
         .then((res) => {
           success?.()
@@ -35,20 +34,27 @@ export function CaptchaModal({
   onSuccess?: () => void
   onCancel?: () => void
 }) {
+  const [loaded, setLoaded] = useState(false)
 
   useEffect(() => {
     if (isServe() || !open) return
+
+    if (loaded) {
+      window.grecaptcha.reset()
+      return
+    }
+
     googleVerify(() => {
       onSuccess?.()
     })
+
+    setLoaded(true)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [open])
 
   return (
     <Modal open={open} footer={null} onCancel={onCancel}>
-      <div className="w-full flex justify-center items-center">
-        <div className="w-full flex justify-center items-center mt-[10px]" id="robot"></div>
-      </div>
+      <div className="w-full flex justify-center items-center mt-[10px] flex-center" id="robot"></div>
     </Modal>
   )
 }
